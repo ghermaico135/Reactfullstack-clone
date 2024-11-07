8543d066b15da439d17fb8e43d8d932bcb649690
ghermaico135 <ghermaico135@gmail.com>
2024-11-07 02:46:37 -0800

    Adding post Api

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+// database
 const db = require("./models")
 
 // Routers
 const postRouter = require("./routes/Posts")
+
 app.use("/posts",postRouter)
 
 db.sequelize.sync().then(()=>{
--- server/routes/Posts.js
+++ server/routes/Posts.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const router = express.Router()
+const {Posts} = require("../models")
 
-router.get("/",(req,res) =>{
-    res.send("Hello world")
+// crud and rest api
+router.get("/", async(req,res) =>{
+    const listOfAllPost = await Posts.findAll()
+    res.json(listOfAllPost)
+})
+
+router.post("/", async (req,res)=>{
+    const post = req.body
+    await Posts.create(post)
+    res.json(post)
 })
 
 module.exports = router
