75971adc34a6adc397a461d1d10991acdb410879
ghermaico135 <ghermaico135@gmail.com>
2024-11-07 01:50:42 -0800

    Adding router

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const app = express()
+
 const db = require("./models")
 
-const app = express()
+// Routers
+const postRouter = require("./routes/Posts")
+app.use("/posts",postRouter)
 
 db.sequelize.sync().then(()=>{
     app.listen( 3001,()=>{
