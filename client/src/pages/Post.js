075c1d3e9aca7a935fd9780fe9095ec31f7a4c6e
ghermaico135 <ghermaico135@gmail.com>
2024-11-14 02:21:38 -0800

    Adding comment api

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -11,15 +11,15 @@ function Post() {
         axios.get(`http://localhost:3001/posts/byId/${id}`).then((response)=>{
                 setPostObject(response.data)
           })
-    },[])
+    })
   return (
     <div className="postPage">
         <div className="leftSide">
-        <div className="title">{postObject.title}</div>
+            <div className="title">{postObject.title}</div>
             <div className="body">{postObject.postText}</div>
             <div className="footer">{postObject.username}</div>
         </div>
-        <div className="rightSide"></div>
+        <div className="rightSide">comment session</div>
     </div>
   )
 }
